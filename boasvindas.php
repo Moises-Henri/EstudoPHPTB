524d8a2aedbda412ea04020ce339f039cc205986
Moises-Henri <moisessouza4699@gmail.com>
2025-05-28 08:20:50 -0300

    Aula21/05

--- boasvindas.php
+++ boasvindas.php
@@ -5,35 +5,45 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
     <script src="js/jquery-3.7.1.min.js" defer></script>
     <script src="js/bootstrap.js" defer></script>
+    
 </head>
 <body>
 
 
 
 <div class="container mt-5">
-    <div class="row   justify-content-center">
+    <div class=" login row   justify-content-center">
         <div class="col-12 col-sm-6 col-md-6 col-lg-6">
-            <form action="" method="POST">
-                <label for="">Digite seu nome: </label><br>
+            <div class="card">
+                <div class="card-header bg-info">
+</div>
+                    <div class="card-body">
+                        <h4> Menu de Login </h4>
+
+                    <form action="" method="POST">
+                <label>Digite seu nome: </label><br>
         
                     <input type="text" name="nome" class="form-control" id="nome"></input>
-        
+                    <label> Digita sua senha: </label>
+                    <input type="password" name="senha" class="form-control" id="senha"></input>
+
+                    <div class="col-12 col-sm-6 col-md-6 col-lg-6"> 
                     <input type="submit" class="btn btn-outline-primary" method="POST"></input>
+                    </div>
                     <br>
-                        <div class="alert  alert-success ">
-                        <h3>
-                        <?php
-                        if(isset($_POST['nome'])){
-                        echo "Seja bem-vindo ". $_POST['nome'];
-                        }else{
-                           echo "Digite seu nome...";                     
-                        }
-                        ?>
-                        </h3>
-                        </div>
+                        
             </form>
+                    </div>
+                        <div class="card-footer bg-info">
+
+                        
+                    </div>
+                
+            </div>
+           
         </div>         
     </div>
 </div>
